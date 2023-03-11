9a72dc5a4633e33abf5cfd6bca0ef51c1fa29171
Khufu737 <111590098+Khufu737@users.noreply.github.com>
2023-03-11 23:50:07 +0200

    Add files via upload

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['201121968769'] //ur owner number
+global.ownername = "khufu" //ur owner name
+global.ytname = "YT: khufu" //ur yt chanel name
+global.socialm = "GitHub: khufu737" //ur github or insta name
+global.location = "eg, g,y" //ur location
 
 //bot bomdy 
-global.ownernomer = "916909137213" //ur number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot MD V6' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.ownernomer = "201121968769" //ur number
+global.premium = ['201121968769'] //ur premium number
+global.botname = 'ᴹᴿ᭄ Bot Whatsapp ×፝֟͜×️' //ur bot name
+global.linkz = "https://chat.whatsapp.com/Ivno4IbIyxMKSgyg6f1dKw" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/@khufualmoz3g974" //ur website to be displayed
+global.botscript = 'https://youtube.com/@khufualmoz3g974' //script link
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.packname = "خوفو ص المزعجين ☺️" //ur sticker watermark packname
+global.author = "201121968769" //ur sticker watermark author
+global.wm = "khufu." //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -40,17 +40,17 @@ global.sp = ''
 
 //messages
 global.mess = {
-    success: 'Here you go!',
-    admin: 'This feature could be used by admins only!',
-    botAdmin: 'Bot Must Be Admin First!',
-    premime: 'Premium Special Features If You Want to Register Type Rent',
-    owner: 'This feature could be used by owner only',
-    group: 'Features Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This feature could be used by bot only',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    success: 'ضن☺️',
+    admin: 'الادمن فقط ',
+    botAdmin: 'لازم اكون ادمن',
+    premime: 'اعضاء المميزين فقط',
+    owner: 'الامر ده ل مالك بوت',
+    group: 'رومات فقط!',
+    private: 'برايفت بس',
+    bot: 'امر ده ل بوت',
+    wait: 'استني...',
+    linkm: 'فين لينك',
+    endLimit: 'تعالي بكره بق ☺️',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
 }
 
